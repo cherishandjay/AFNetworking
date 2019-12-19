eb5f7983ee9659277f086d7562dbe0284405b4c0
Jeremy <you@1509028992@qq.com>
2019-12-19 22:47:17 +0800

    自己实现网络请求

--- Example/循环引用/ViewController.m
+++ Example/循环引用/ViewController.m
@@ -24,9 +24,18 @@
     
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSURLSession* session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue] ];
-     
-    NSURLSessionDataTask* task = [session dataTaskWithURL:[NSURL URLWithString:@"https://1z4-devmng2.myzmodo.com/zmd/common/globalconfig?clienttype=1&language=en&platform=2"] ];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionTask* task = nil;
+    NSURL* url = [NSURL URLWithString:@"https://1z4-devmng2.myzmodo.com/zmd/common/globalconfig"];
+//    task = [session dataTaskWithURL:url];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    request.HTTPBody = [@"clienttype=1&language=en&platform=2" dataUsingEncoding:NSUTF8StringEncoding];
+    task = [session dataTaskWithRequest:request];
+//  task = [session dataTaskWithURL:[NSURL URLWithString:@"https://1z4-devmng2.myzmodo.com/zmd/common/globalconfig?clienttype=1&language=en&platform=2"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//           NSDictionary * result = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+//           NSLog(@"字符串转到data，再转成json%@",result);
+//       }];
     [task resume];
 
 }
@@ -34,28 +43,36 @@
 #pragma mark -- NSURLSessionDataDelegate
 
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition disposition))completionHandler{
-    
+ 
+    completionHandler(NSURLSessionResponseAllow);
 }
 
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
 {
-    NSLog(@"进度：%lld",dataTask.countOfBytesReceived/dataTask.countOfBytesExpectedToReceive);
     [self.mutableData appendData:data];
+////    if ((dataTask.countOfBytesReceived - dataTask.countOfBytesExpectedToReceive) = 0) {
+////
+////    }
+// NSLog(@"进度：%lld",dataTask.countOfBytesReceived/dataTask.countOfBytesExpectedToReceive);
+    
 }
 
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask willCacheResponse:(NSCachedURLResponse *)proposedResponse completionHandler:(void (^)(NSCachedURLResponse *cachedResponse))completionHandler
 {
-    NSLog(@"datatask:%@",dataTask );
+     if (completionHandler) {
+        completionHandler(NULL);
+    }
 }
 
 
 #pragma mark-- NSURLSessionDelegate
-/* The last message a session receives.  A session will only become
- * invalid because of a systemic error or when it has been
- * explicitly invalidated, in which case the error parameter will be nil.
- */
-- (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(nullable NSError *)error{
-    
+ 
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task
+didCompleteWithError:(nullable NSError *)error
+{
+    if (error == nil) {
+          NSDictionary * result = [NSJSONSerialization JSONObjectWithData:self.mutableData options:NSJSONReadingMutableLeaves error:nil];
+        NSLog(@"字符串转到data，再转成json%@",result);    }
 }
 
 /* If implemented, when a connection level authentication challenge
@@ -67,10 +84,32 @@
  * behavior will be to use the default handling, which may involve user
  * interaction.
  */
-- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
- completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler{
-    
-}
+//- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+// completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler{
+//    NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+//    __block NSURLCredential *credential = nil;
+////    if (self.sessionDidReceiveAuthenticationChallenge) {
+////           disposition = self.sessionDidReceiveAuthenticationChallenge(session, challenge, &credential);
+////       } else {
+////           if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+////               if ([self.securityPolicy evaluateServerTrust:challenge.protectionSpace.serverTrust forDomain:challenge.protectionSpace.host]) {
+//                   credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+////                   if (credential) {
+////                       disposition = NSURLSessionAuthChallengeUseCredential;
+////                   } else {
+////                       disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+////                   }
+////               } else {
+//                   disposition = NSURLSessionAuthChallengeCancelAuthenticationChallenge;
+////               }
+////           } else {
+////           }
+////       }
+//
+//       if (completionHandler) {
+//           completionHandler(disposition, credential);
+//       }
+//}
 
 /* If an application has received an
  * -application:handleEventsForBackgroundURLSession:completionHandler:
@@ -80,10 +119,10 @@
  * completion handler, or to begin any internal updates that will
  * result in invoking the completion handler.
  */
-- (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session
-{
-    
-}
+//- (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session
+//{
+//
+//}
 
 
 
